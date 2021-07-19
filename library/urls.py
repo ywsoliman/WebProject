edccd66cdf87f720be7b6b108df4fea507364a73
Youssef W. Soliman <ywsoliman@gmail.com>
2021-07-19 18:28:06 +0200

    V1.0

---
+++ library/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='pages/logout.html'), name='logout'),
+    path('search/', views.search, name='search'),
+    path('profile/', views.profile, name='profile'),
+    path('browseall/', views.browseall, name='browseall')
+    # path('updateform/', views.updateform, name='updateform')
+]
---
+++ library/views.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created successfully!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'pages/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            messages.success(request, f'Your account information has been updated!')
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'pages/profile.html', context)
+
+def search(request):
+    return render(request, 'pages/booksearch.html')
+
+def browseall(request):
+    return render(request, 'pages/browseall.html')
